b6b979c63ccc1c7bcc1dad97de1e079e9707689d
lifez999@gmail.com <lifez999@gmail.com>
2014-02-07 00:52:22 +0700

    Refactor

--- codeforces/231a.rb
+++ codeforces/231a.rb
@@ -1,9 +1,8 @@
-problems = gets.strip.to_i
 canSolve = 0
-(1..problems).each {|x|
+gets.to_i.times.map {
   who_can = gets.split(" ").map {|x| x.to_i}
-  if who_can.count(1) >= 2
-    canSolve += 1
+  if who_can.count(1) >=2
+    canSolve +=1
   end
 }
 puts canSolve
